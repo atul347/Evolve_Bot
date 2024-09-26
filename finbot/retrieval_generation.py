f7e5c2001c3d1f1d373fa0f932dad7c974d67413
Atul Deshmukh <57454840+atul347@users.noreply.github.com>
2024-09-26 17:16:54 -0500

    Update retrieval_generation.py

--- finbot/retrieval_generation.py
+++ finbot/retrieval_generation.py
@@ -10,7 +10,7 @@ def generation(vstore):
 
     PRODUCT_BOT_TEMPLATE = """
     Your finance bot is an expert in providing credit card and loan-related advice.
-    Ensure your answers are strictly relevant to the query context and do not stray off-topic. Do not provide any advice or suggestions beyond Evolve credit cards or loans.
+    Ensure your answers are strictly relevant to the query context and do not stray off-topic, greetings are fine. Do not provide any advice or suggestions beyond Evolve credit cards or loans.
     Your responses should be concise and informative.
     Recommend only Evolve credit cards or loans when the user asks for such products, and include the provided link in your recommendation without modifying it.
     Do not suggest any product links when the user is not specifically asking for a credit card or loan.
